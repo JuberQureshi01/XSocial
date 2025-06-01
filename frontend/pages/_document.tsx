16c2221b671379256c55c23e90d9468fc1cc960d
Juber Qureshi <142721071+JuberQureshi01@users.noreply.github.com>
2025-06-01 21:57:33 +0530

    Update _document.tsx

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" type="image/png" />
+      </Head>
+      <title>XSocial</title>
       <body className="antialiased">
         <Main />
         <NextScript />
