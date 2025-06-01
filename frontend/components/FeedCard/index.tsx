085beb5f136b472a06f7c577e4befbd0ea21bf7d
Juber Qureshi <142721071+JuberQureshi01@users.noreply.github.com>
2025-06-01 19:38:47 +0530

    Update index.tsx

--- frontend/components/FeedCard/index.tsx
+++ frontend/components/FeedCard/index.tsx
@@ -18,31 +18,36 @@ const FeedCard: React.FC<FeedCardProops> = (props) => {
     <div className="border-t-[0.5px] border-zinc-700 p-5 hover:bg-zinc-900 transition-all cursor-pointer w-fit flex flex-wrap overflow-x-hidden">
       <div className="grid grid-cols-12 gap-2 ">
         <div className="col-span-1">
-          <Link href={data?.author?.id ||"/"}><Image
-            src={
-              data?.author?.profileImageUrl
-                ? data.author.profileImageUrl
-                : data.imageURl
-            }
-            alt="user-image"
-            className="rounded-full"
-            height={50}
-            width={50}
-          /></Link>
+          <Link href={data?.author?.id || "/"}>
+            {data.author?.profileImageUrl && (
+              <Image
+                src={data.author.profileImageUrl}
+                alt="user-image"
+                className="rounded-full"
+                height={50}
+                width={50}
+              />
+            )}
+          </Link>
         </div>
         <div className="col-span-11">
-          <Link href={data.author?.id}><h5>{data.author?.firstName} {data.author?.lastName}</h5></Link>
-          <p>{data.content}
-          </p>
-            {data.imageURl&&<Image
-            src={
-              data.imageURl
-            }
-            alt="user-image"
-            className=" flex justify-center py-2 h-[250px] w-[390px] rounded-md"
-            height={80}
-            width={250}
-          />}
+          { data.author&&
+            <Link href={data.author?.id}>
+              <h5>
+                {data.author?.firstName} {data.author?.lastName}
+              </h5>
+            </Link>
+          }
+          <p>{data.content}</p>
+          {data.imageURl && (
+            <Image
+              src={data.imageURl}
+              alt="user-image"
+              className=" flex justify-center py-2 h-[250px] w-[390px] rounded-md"
+              height={80}
+              width={250}
+            />
+          )}
           <div className="flex justify-between mt-5 ">
             <div className="flex items-center gap-1 text-md hover:text-sky-300  ">
               <FaRegComment />
